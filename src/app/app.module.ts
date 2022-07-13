20889e4fbc722458df33db89a4b560eb8ec93c11
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2022-07-13 15:30:23 -0300

    estrutura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    PerfilComponent,
+    CatalogoComponent
   ],
   imports: [
     BrowserModule,
