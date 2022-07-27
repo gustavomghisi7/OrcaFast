241efb224207c9119e62a9bb074afdf83dcd19bf
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2022-07-27 16:36:57 -0300

    output usuário

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UsuariosService } from '../../services/usuarios.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private usuarioService: UsuariosService) { }
 
   ngOnInit(): void {
+  }
+
+  userLogado: any = this.usuarioService.getDadosToken()
+
+  logout(): void {
+    localStorage.removeItem('usertoken')
+    this.userLogado = ''
+    window.location.reload()
   }
 
 }
