b8e254aa8ee2311ba9cc8f7c2f9f2138029a9fe2
Mikaelen Oliveira <mikaelennilo@gmail.com>
2022-07-27 21:46:59 -0300

    Alteração header / Authguard de Adm

--- src/app/guards/auth-guard-user.service.ts
+++ src/app/guards/auth-guard-user.service.ts
@@ -9,11 +9,11 @@ export class AuthGuardUserService implements CanActivate {
   constructor(private router: Router, private service: UsuariosService) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     var user = this.service.getDadosToken()
-  
+
     const routeParams = route.paramMap
     const idUsuario = Number(routeParams.get('idusuario'))
 
-    if (user.perfil == "USUARIO" && user.id == idUsuario) {
+    if ((user.perfil == "USUARIO" && user.id == idUsuario) || user.perfil == 'ADM') {
       return true
     } else {
       this.router.navigate([''])
