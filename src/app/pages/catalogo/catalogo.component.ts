6eada464020e28f45c9d2437727d2c5ad2cfc0dc
Katiane Griza <katygriza@hotmail.com>
2022-08-31 21:36:21 -0300

    tratamento do dado produto

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -13,12 +13,23 @@ export class CatalogoComponent implements OnInit {
   idUsuario: number = 0
 
   produtos: any = []
-  
+
   orcamento: any = {}
 
   listaIdProdutos: any = {}
 
   listaSelecao: any = []
+
+  objetoSelecao: any = {
+
+    "quantidade": 1.0,
+    "preco": 0.00,
+    "produto": {"id": 0},
+    "orcamento": {"id": 0}
+
+  }
+
+
 
   constructor(private service: ProdutosService, private route: ActivatedRoute) { }
 
@@ -38,43 +49,44 @@ export class CatalogoComponent implements OnInit {
     produto.selecionado = !(produto.selecionado || false);
     this.listaSelecao = this.produtos.filter((prod: any) => prod.selecionado)
   }
-//Nesta função vai ter, 
-// gerar id do orçamento ok
-// pegar id do usuário URL ok
-// tratar o dado do Produto o
-// juntar tudo em objeto
-// enviar para URL seleção.
+  //Nesta função vai ter, 
+  // gerar id do orçamento ok
+  // pegar id do usuário URL ok
+  // tratar o dado do Produto ok
+  // juntar tudo em objeto
+  // enviar para URL seleção.
 
-  salvarSelecao(){
+  salvarSelecao() {
     const routeParams = this.route.snapshot.paramMap;
     this.idUsuario = Number(routeParams.get('idusuario'))
-    //console.log(this.idUsuario)
-    //console.log(this.listaSelecao);
-    
+
     let dadosOrcamento = {
-     // usuario: this.idUsuario
-    usuario: {id: 2}
+      usuario: { id: this.idUsuario }
     }
 
     this.service.criarOrcamento(dadosOrcamento).subscribe(data => {
       this.orcamento = data
       //console.log(this.orcamento.id);
     })
-    for (let i = 0; i< this.listaSelecao.length; i++){
+    for (let i = 0; i < this.listaSelecao.length; i++) {
       this.listaIdProdutos = {
         id: this.listaSelecao[i].id
       }
-    console.log(this.listaIdProdutos)
-  }
+      console.log(this.listaIdProdutos)
+    }
 
-  
-    
+    this.objetoSelecao.quantidade = 1
+    this.objetoSelecao.preco = 0.00
+    this.objetoSelecao.produto.id = this.listaIdProdutos
+    this.objetoSelecao.orcamento.id = this.orcamento.id
+
     return this.listaSelecao;
   }
-
 
   ngOnDestroy(): void {
     this.inscricaoGetProdutos.unsubscribe;
   }
+  
+
 
 }
