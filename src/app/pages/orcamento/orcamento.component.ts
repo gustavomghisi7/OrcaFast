2727523a227d6252879712647e7b16e68a899491
mariabelencaldez <mariabelencaldez@gmail.com>
2022-09-10 11:14:30 -0300

    navegabilidade botão voltar desde orçamento para catálogo

--- src/app/pages/orcamento/orcamento.component.ts
+++ src/app/pages/orcamento/orcamento.component.ts
@@ -1,12 +1,14 @@
 import { IProduto, IProdutoFornecedor } from './../../types';
 import { ISelecao } from 'src/app/types';
+import { UsuariosService } from '../../services/usuarios.service';
 import { ProdutosService } from 'src/app/services/produtos.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import * as XLSX from 'xlsx';
-
 import { Subscription } from 'rxjs';
+
+declare const M: any
 
 @Component({
   selector: 'app-orcamento',
@@ -14,18 +16,33 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./orcamento.component.css'],
 })
 export class OrcamentoComponent implements OnInit {
+  idUsuario: number = 0
+  usuario: any = {}
+
   constructor(
+    private serviceUsuario:UsuariosService,
     private service: ProdutosService,
     private route: ActivatedRoute
   ) {}
 
+  inscricaoGetUmUsuario: any = Subscription;
   inscricaoGet: any = Subscription;
   fileName = 'orcamentoExcel.xlsx';
 
   ngOnInit(): void {
+
+    M.updateTextFields()
+
+    M.AutoInit()
+
     const routeParams = this.route.snapshot.paramMap;
     const idOrcamento = Number(routeParams.get('idorcamento'));
     this.mostrarOrcamentoById(idOrcamento);
+
+    this.idUsuario = Number(routeParams.get('idusuario'))
+    this.inscricaoGetUmUsuario = this.serviceUsuario.getUmUsuario(this.idUsuario).subscribe(data => {
+    this.usuario = data
+  })
   }
 
   selecao: ISelecao[] = [];
