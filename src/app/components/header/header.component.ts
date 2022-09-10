4c2b852f23c9f4ce90ee73f24d3c1658b2896db8
mariabelencaldez <mariabelencaldez@gmail.com>
2022-09-10 10:35:30 -0300

    navegaçao desde header ao catalogo com id usuario

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { UsuariosService } from '../../services/usuarios.service';
+
+declare const M: any
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
-  constructor(private usuarioService: UsuariosService) { }
+  idUsuario: number = 0
+  usuario: any = {}
+
+  constructor(private usuarioService: UsuariosService, private route: ActivatedRoute) { }
+
+  inscricaoGetUmUsuario: any = Subscription;
+
+  ngOnInit(): void {
+    
+  
+
+    M.AutoInit()
+    const routeParams = this.route.snapshot.paramMap;
+
+    this.idUsuario = Number(routeParams.get('idusuario'))
+    this.inscricaoGetUmUsuario = this.usuarioService.getUmUsuario(this.idUsuario).subscribe(data => {
+    this.usuario = data
+  })}
 
   userLogado: any = this.usuarioService.getDadosToken()
 
