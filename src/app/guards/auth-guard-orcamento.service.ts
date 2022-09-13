fe9184bf3dadbf87de6547a35915f017b6abcd72
Maria Ester Veiga <esterdcveiga@gmail.com>
2022-09-13 15:58:29 -0300

    correção em auth-guard-orcamento

--- src/app/guards/auth-guard-orcamento.service.ts
+++ src/app/guards/auth-guard-orcamento.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ProdutosService } from '../services/produtos.service';
+import { UsuariosService } from '../services/usuarios.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardOrcamentoService implements CanActivate{
 
-  constructor(private router: Router, private service: ProdutosService) { }
+  constructor(private router: Router, private service: ProdutosService, private services: UsuariosService) { }
   
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    var user = this.services.getDadosToken()
     const routeParams = route.paramMap
     const idOrcamento = Number(routeParams.get('idorcamento'))
     const idUsuario = Number(routeParams.get('idusuario'))
@@ -21,12 +23,18 @@ export class AuthGuardOrcamentoService implements CanActivate{
         if (orcamento.usuario.id == idUsuario) {
           return true
         }else{
-          //this.router.navigate([''])
+          this.router.navigate([''])
           //window.location.reload()
           alert('Usuário sem acesso')
           return false
         }
     })
+    if (user.id != idUsuario) {
+      this.router.navigate([''])
+          //window.location.reload()
+      alert('Usuário sem acesso')
+      return false
+    }
 
     return true
   }
